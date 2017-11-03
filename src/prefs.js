603beaea9e85956e7f2907f3a32142ff4a456634
Davi da Silva Böger <dsboger@gmail.com>
2017-11-03 18:27:32 -0200

    Use libdazzle shortcuts goodness if available

--- src/prefs.js
+++ src/prefs.js
@@ -20,6 +20,12 @@
 const Gio = imports.gi.Gio;
 const Gtk = imports.gi.Gtk;
 const Gettext = imports.gettext;
+var Dazzle;
+try {
+	Dazzle = imports.gi.Dazzle;
+} catch (e) {
+	// continue without Dazzle
+}
 
 const ExtensionUtils = imports.misc.extensionUtils;
 const Config = imports.misc.config;
@@ -43,18 +49,28 @@ function _getExtensionSettingsToplevel() {
 
 function _editShortcut(row, shortcutKey, shortcutSummary, settings) {
 	let toplevel = _getExtensionSettingsToplevel();
-	let dialog = new Gtk.MessageDialog({transient_for: toplevel, message_type: Gtk.MessageType.QUESTION, buttons: Gtk.ButtonsType.OK_CANCEL,
-			title: GCC_("Set Shortcut")});
-	dialog.set_markup(GCC_("Enter new shortcut to change <b>%s</b>.").format(shortcutSummary));
+	if (Dazzle) {
+		let dialog = new Dazzle.ShortcutAccelDialog({transient_for: toplevel, shortcut_title: shortcutSummary});
+		if (dialog.run() == Gtk.ResponseType.ACCEPT) {
+			if (dialog.accelerator) {
+				settings.set_strv(shortcutKey, [dialog.accelerator]);
+			} else {
+				settings.set_strv(shortcutKey, []);
+			}
+		}
+		dialog.destroy();
+	} else {
+		let dialog = new Gtk.MessageDialog({transient_for: toplevel, message_type: Gtk.MessageType.QUESTION, buttons: Gtk.ButtonsType.OK_CANCEL,
+				title: GCC_("Set Shortcut"), text: GCC_("Enter new shortcut to change <b>%s</b>.").format(shortcutSummary), use_markup: true});
 
-	let entry = new Gtk.Entry({visible: true});
-	entry.text = settings.get_strv(shortcutKey).toString();
-	dialog.get_message_area().pack_start(entry, false, false, 0);
-	if (dialog.run() == Gtk.ResponseType.OK) {
-		let newShortcuts = entry.text.split(',');
-		settings.set_strv(shortcutKey, newShortcuts);
+		let entry = new Gtk.Entry({visible: true});
+		entry.text = settings.get_strv(shortcutKey).toString();
+		dialog.get_message_area().pack_start(entry, false, false, 0);
+		if (dialog.run() == Gtk.ResponseType.OK) {
+			settings.set_strv(shortcutKey, entry.text.split(','));
+		}
+		dialog.destroy();
 	}
-	dialog.destroy();
 }
 
 function _createShortcutRow(shortcutKey, settings) {
@@ -76,15 +92,28 @@ function _createShortcutRow(shortcutKey, settings) {
 			shortcutBox.pack_start(shortcutNameLabel, true, true, 0);
 		}
 		{
-			let switchActionsShortcut = settings.get_strv(shortcutKey).toString();
-			let shortcutValueLabel = Gtk.Label.new(switchActionsShortcut);
-			shortcutValueLabel.visible = true;
-			shortcutValueLabel.get_style_context().add_class('dim-label');
-			let settingChangedId = settings.connect("changed::" + shortcutKey, function() {
-						shortcutValueLabel.label = settings.get_strv(shortcutKey).toString();
-					});
-			row.connect('destroy', function() { settings.disconnect(settingChangedId); });
-			shortcutBox.pack_start(shortcutValueLabel, false, false, 0);
+			let shortcuts = settings.get_strv(shortcutKey);
+			if (Dazzle) {
+				let shortcut = shortcuts.length > 0 ? shortcuts[0] : null;
+				let shortcutValueLabel = new Dazzle.ShortcutLabel({ accelerator: shortcut });
+				shortcutValueLabel.visible = true;
+				shortcutValueLabel.get_style_context().add_class('dim-label');
+				let settingChangedId = settings.connect("changed::" + shortcutKey, function() {
+							let shortcuts = settings.get_strv(shortcutKey);
+							shortcutValueLabel.accelerator = shortcuts.length > 0 ? shortcuts[0] : null;
+						});
+				row.connect('destroy', function() { settings.disconnect(settingChangedId); });
+				shortcutBox.pack_start(shortcutValueLabel, false, false, 0);
+			} else {
+				let shortcutValueLabel = Gtk.Label.new(shortcuts.toString());
+				shortcutValueLabel.visible = true;
+				shortcutValueLabel.get_style_context().add_class('dim-label');
+				let settingChangedId = settings.connect("changed::" + shortcutKey, function() {
+							shortcutValueLabel.label = settings.get_strv(shortcutKey).toString();
+						});
+				row.connect('destroy', function() { settings.disconnect(settingChangedId); });
+				shortcutBox.pack_start(shortcutValueLabel, false, false, 0);
+			}
 		}
 		{
 			let resetShortcutButton = Gtk.Button.new_from_icon_name('edit-clear-symbolic', Gtk.IconSize.BUTTON);
